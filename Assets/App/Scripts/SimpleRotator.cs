ea2c646d90ed38cf7d33e05e94967f94b58a0563
Joost van Schaik <joostvanschaik@outlook.com>
2017-04-18 07:49:54 +0200

    Initial checkin - thanks to Travis Rothbloom for reminding me ;)

---
+++ Assets/App/Scripts/SimpleRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SimpleRotator : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        LeanTween.rotateAround(gameObject, Vector3.up, 360, 10f).setLoopClamp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
